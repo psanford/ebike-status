2430eaa0701d1d0bcc79cfe3ed483036ead2531d
Peter Sanford <psanford@sanford.io>
2018-05-10 17:25:40 -0700

    Add license
    Remove incorrect copyright boilerplate.

--- main.go
+++ main.go
@@ -1,10 +1,3 @@
-// Copyright (c) 2018, RetailNext, Inc.
-// This material contains trade secrets and confidential information of
-// RetailNext, Inc.  Any use, reproduction, disclosure or dissemination
-// is strictly prohibited without the explicit written permission
-// of RetailNext, Inc.
-// All rights reserved.
-
 package main
 
 import (
